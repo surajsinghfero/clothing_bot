8d2d52eded37bd87de9e38b37e06c42d3f11be1b
Suraj Singh <suraj32159@gmail.com>
2022-02-19 13:24:59 +0530

    change14

--- myprojectect/settings.py
+++ myprojectect/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'social/static'
